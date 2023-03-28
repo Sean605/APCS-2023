ede4485b058a54fca55b8600cd95aa4168c352cb
Sean Dyer <127899953+Sean605@users.noreply.github.com>
2023-03-28 21:17:44 +0000

    Finished

--- unit10/PiDay.java
+++ unit10/PiDay.java
@@ -3,20 +3,23 @@ package unit10;
 public class PiDay {
     
     public static double piDay(int n, boolean subtract) {
-        if(n == 1000){
-            
-        }
-        else{
-            
+        if(n >= 1000){
+            return 0.0;
+        } else{
+            if(subtract){
+                return -4.0/n + piDay(n + 2, false);
+            } else{
+                return 4.0/n + piDay(n + 2, true);
+            }
         }
     }
   
     public static void main(String args[]) {
         //  Gregory–Leibniz
-        //double pi = piDay(1, false);
+        double pi = piDay(1, false);
   
         // Nilakantha
-        double pi = 3.0 + piDay(3, false);
+        //double pi = 3.0 + piDay(3, false);
   
         System.out.println("pi = " + pi);
     }
