4cdfcafc699ee5f5f3d3a2f80f1332f46f3724b5
Sean Dyer <127899953+Sean605@users.noreply.github.com>
2023-05-07 02:16:43 +0000

    Crossword.java finished

--- unit11/Crossword.java
+++ unit11/Crossword.java
@@ -50,11 +50,13 @@ public class Crossword {
     public Crossword(boolean[][] blackSquares) {
         puzzle = new Square[blackSquares.length][blackSquares[0].length];
         int num = 1;
-        for(int r = 0; r < blackSquares.length; r++){
-            for(int c = 0; c < blackSquares[0].length; c++){
-                int n;
+        for(int r = 0; r < puzzle.length; r++){
+            for(int c = 0; c < puzzle[0].length; c++){
                 if(toBeLabeled(r, c, blackSquares)){
-                    
+                    puzzle[r][c] = new Square(false, num);
+                    num++;
+                } else{
+                    puzzle[r][c] = new Square(blackSquares[r][c], 0);
                 }
             }
         }
